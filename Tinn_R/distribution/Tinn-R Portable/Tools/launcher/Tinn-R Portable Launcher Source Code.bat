ba769cdea67761199e816b681b1634be772ace06
fariasphil <philfarias@gmail.com>
2015-07-01 21:41:38 -0300

    Tinn-R PortableApps Edition Bug's fix.

---
+++ Tinn_R/distribution/Tinn-R Portable/Tools/launcher/Tinn-R Portable Launcher Source Code.bat
@@ -0,0 +1,10 @@
+::  Author: Philipe Silva Farias
+:: 	File Version: 1.0.0
+::  Date: 2015/07/01
+::	Description: This script file has been written to call Tinn-R Portable bin file.
+:: Change the nine line by the correct bin file patch.
+
+@echo off
+title Tinn-RPortable Launcher
+call "App\binPortable\Tinn-R Portable.exe" 
+exit
